39d19aba1f880a6b8b617c8a599b44594cf864ab
Furkan Ekiz <furkanekiz1453@gmail.com>
2025-09-28 13:31:14 +0300

    s

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,1 +1,11 @@
-ty
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  server: {
+    hmr: {
+      // Vite'in HMR bağlantısı için özel bir yol tanımlıyoruz.
+      // Tarayıcı artık wss://localhost/vite-hmr adresine bağlanmaya çalışacak.
+      path: '/vite-hmr/'
+    }
+  }
+})
